801cdfe04daf39371aa15431e5f229be953c6a15
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-12 15:29:20 +0100

    Create player.h

---
+++ inc/player.h
@@ -0,0 +1,10 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+typedef struct {
+    float x;        // Player x position
+    float y;        // Player y position
+    float angle;    // Player viewing angle
+} Player;
+
+#endif // PLAYER_H
