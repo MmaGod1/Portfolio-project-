d3a2fe092669716c0d5e2496f64bba086c679d66
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-12 19:43:15 +0100

    Update and rename constants.h to camera.h

---
+++ inc/camera.h
@@ -0,0 +1,9 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+extern float camera_x;
+extern float camera_y;
+extern float camera_angle;
+extern float camera_fov;
+
+#endif // CAMERA_H
--- inc/constants.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-
-#endif // CONSTANTS_H
