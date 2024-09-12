f7ae9bed0218274635c3459d0a42b62e47979c27
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-12 15:24:47 +0100

    Update raycasting.c

--- src/raycasting.c
+++ src/raycasting.c
@@ -1,10 +1,23 @@
 #include <SDL2/SDL.h>
+#include <math.h>
 #include "init.h"
 
-/* Draws walls and ground using raycasting */
-void draw(SDL_Renderer *renderer) {
+/* Define constants for the maze and display */
+#define MAP_WIDTH 10
+#define MAP_HEIGHT 10
+#define CELL_SIZE 80
+#define FOV 60  // Field of view in degrees
+#define VIEW_DIST 1000 // Maximum view distance
+
+/* Player position and orientation */
+typedef struct {
+    float x, y; // Player position
+    float angle; // Player viewing angle
+} Player;
+
+void draw(SDL_Renderer *renderer, Player player) {
     /* Define the map (example map size 10x10) */
-    int map[10][10] = {
+    int map[MAP_HEIGHT][MAP_WIDTH] = {
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
         {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
         {1, 0, 1, 1, 1, 1, 1, 0, 0, 1},
@@ -17,45 +30,52 @@ void draw(SDL_Renderer *renderer) {
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
     };
 
-    /* Define the size of each cell */
-    const int cellWidth = 80;
-    const int cellHeight = 60;
-
     /* Clear screen */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  /* Set color to black for background */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  /* Black background */
     SDL_RenderClear(renderer);
 
-    /* Draw walls */
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
-            SDL_Rect rect = {x * cellWidth, y * cellHeight, cellWidth, cellHeight};
+    /* Cast rays to draw walls */
+    for (int x = 0; x < 640; x++) {  // Assuming screen width is 640
+        // Calculate the angle of the ray
+        float rayAngle = (player.angle - FOV / 2.0f) + ((float)x / 640.0f) * FOV;
 
-            if (map[y][x] == 1) {
-                /* Determine wall orientation */
-                int isVertical = (x > 0 && map[y][x-1] == 1) || (x < 9 && map[y][x+1] == 1);
-                int isHorizontal = (y > 0 && map[y-1][x] == 1) || (y < 9 && map[y+1][x] == 1);
+        // Cast the ray
+        float rayX = player.x;
+        float rayY = player.y;
+        float stepSize = 0.1f;
+        float distanceToWall = 0.0f;
+        int hit = 0;
 
-                /* Set color based on orientation */
-                if (isVertical) {
-                    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  /* Red for North-South walls */
-                } else if (isHorizontal) {
-                    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);  /* Blue for East-West walls */
-                }
+        while (distanceToWall < VIEW_DIST) {
+            rayX += cosf(rayAngle) * stepSize;
+            rayY += sinf(rayAngle) * stepSize;
+            distanceToWall += stepSize;
 
-                SDL_RenderFillRect(renderer, &rect);
+            int mapX = (int)(rayX / CELL_SIZE);
+            int mapY = (int)(rayY / CELL_SIZE);
+
+            if (mapX < 0 || mapX >= MAP_WIDTH || mapY < 0 || mapY >= MAP_HEIGHT) {
+                hit = 1;  // Ray has gone out of bounds
+                break;
+            }
+
+            if (map[mapY][mapX] == 1) {
+                hit = 1;  // Wall hit
+                break;
             }
         }
-    }
 
-    /* Draw ground/ceiling */
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);  /* Set color to green for ground/ceiling */
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
-            if (map[y][x] == 0) {
-                SDL_Rect rect = {x * cellWidth, y * cellHeight, cellWidth, cellHeight};
-                SDL_RenderFillRect(renderer, &rect);
-            }
+        // Draw the wall slice
+        int wallHeight = (int)(CELL_SIZE / distanceToWall * 200);  // Scale factor for height
+        SDL_Rect rect = {x, 300 - wallHeight / 2, 1, wallHeight};  // Assuming screen height is 600
+
+        if (hit) {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // White color for walls
+        } else {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  // Black color if no wall hit
         }
+
+        SDL_RenderFillRect(renderer, &rect);
     }
 
     /* Present the renderer */
