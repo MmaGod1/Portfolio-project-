ac3e60582a8e6f7cd4b3303772c3ce4b03baf969
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-17 09:02:22 +0100

    Create map.c

---
+++ map.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "SDL2/SDL.h"
+#include <stdbool.h>
+#include "const.h"
+
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define MAP_WIDTH 24
+#define MAP_HEIGHT
+
+int maze_map[MAP_WIDTH] [MAP_HEIGHT] = {
