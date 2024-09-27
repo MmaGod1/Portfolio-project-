1025173c2585611fd4e3da3a6d27df80504e70b0
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-27 10:06:06 +0100

    Update load_texture.c

--- src/load_texture.c
+++ src/load_texture.c
@@ -1,19 +1,18 @@
 #include "raycasting.h"
 
-
 /**
  * load_texture - Loads a texture from a file and creates an SDL texture.
+ * @renderer: The SDL_Renderer used to create the texture.
+ * @filename: The path to the image file to load.
  *
  * This function loads an image from the specified filename into an SDL_Surface
  * and then creates an SDL_Texture from that surface. It also frees the surface
  * once the texture has been created. If the image cannot be loaded, it logs
  * an error and returns a Texture with a NULL pointer and zero dimensions.
  *
- * @filename: The path to the image file to load.
- *
- * Return: Textured structure containing the loaded texture and its dimensions.
+ * Return: Texture structure containing the loaded texture and its dimensions.
  */
-Texture load_texture(const char *filename)
+Texture load_texture(SDL_Renderer *renderer, const char *filename)
 {
 	Texture texture, temp;
 	SDL_Surface *surface = IMG_Load(filename);
@@ -29,21 +28,20 @@ Texture load_texture(const char *filename)
 	}
 
 	texture.texture = SDL_CreateTextureFromSurface(renderer, surface);
-	SDL_FreeSurface(surface);
 	texture.width = surface->w;
 	texture.height = surface->h;
+	SDL_FreeSurface(surface);
 	return (texture);
 }
 
-
 /**
  * load_resources - Loads the map and textures required for the game.
- *
+ * @renderer: The SDL_Renderer used for creating textures.
  * @mapfile: The name of the map file to load.
  *
  * Return: 0 on success, or 1 if an error occurs.
  */
-int load_resources(char *mapfile)
+int load_resources(SDL_Renderer *renderer, char *mapfile)
 {
 	if (load_map(mapfile, maze_map) != 0)
 	{
@@ -52,11 +50,11 @@ int load_resources(char *mapfile)
 	}
 
 	/* Load textures */
-	wallTextures[0] = load_texture("./images/wall1.jpg");
-	wallTextures[1] = load_texture("./images/wall2.jpg");
-	wallTextures[2] = load_texture("./images/wall3.jpg");
-	wallTextures[3] = load_texture("./images/wall4.jpg");
-	floorTexture = load_texture("./images/floor.jpg");
+	wallTextures[0] = load_texture(renderer, "./images/wall1.jpg");
+	wallTextures[1] = load_texture(renderer, "./images/wall2.jpg");
+	wallTextures[2] = load_texture(renderer, "./images/wall3.jpg");
+	wallTextures[3] = load_texture(renderer, "./images/wall4.jpg");
+	floorTexture = load_texture(renderer, "./images/floor.jpg");
 
 	return (0);
 }
