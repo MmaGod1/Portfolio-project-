e4f8e0da28a474ecec81ad4b1c46db28f852b1e7
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-27 18:14:57 +0100

    Update draw.c

--- preserve/src/draw.c
+++ preserve/src/draw.c
@@ -1,81 +1,6 @@
 #include "raycasting.h"
 
 /**
- * draw_sky - Renders the sky in the game window.
- *
- * This function sets the draw color to a night blue and fills
- * the top half of the screen with a rectangle representing the sky.
- *
- * Return: void
- */
-void draw_sky(void)
-{
-	SDL_Rect skyRect;
-
-	skyRect.x = 0;
-	skyRect.y = 0;
-	skyRect.w = SCREEN_WIDTH;
-	skyRect.h = SCREEN_HEIGHT / 2;
-
-	SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
-	SDL_RenderFillRect(renderer, &skyRect);
-}
-
-/**
- * draw_mini_map - Renders a mini-map in the game window.
- *
- * This function draws a mini-map showing the maze layout and the player's
- * position. It highlights walls in red, free spaces in white, and marks
- * the player's position in green. Additionally, it visualizes the player's
- * line of sight with blue rays.
- *
- * @player: Player structure -- the player's position and angle.
- * @showMap: Boolean flag indicating whether to display the mini-map.
- *
- * Return: void
- */
-void draw_floor(Player *player)
-{
-	int y, x, texX, texY;
-	float rayAngle, floorX, floorY, floorDist;
-	SDL_Rect srcRect, dstRect;
-
-	/* Loop over the lower half of the screen */
-	for (y = SCREEN_HEIGHT / 2; y < SCREEN_HEIGHT; y++)
-	{
-		floorDist = SCREEN_HEIGHT / (2.0f * y - SCREEN_HEIGHT);
-
-		/* Loop over the screen width */
-		for (x = 0; x < SCREEN_WIDTH; x++)
-		{
-			rayAngle = player->angle - (FOV / 2) + (FOV * x / SCREEN_WIDTH);
-			floorX = player->x + cos(rayAngle) * floorDist;
-			floorY = player->y + sin(rayAngle) * floorDist;
-			texX = (int)(floorX * floorTexture.width) % floorTexture.width;
-			texY = (int)(floorY * floorTexture.height) % floorTexture.height;
-
-			/* Ensure texture coordinates are valid */
-			if (texX < 0)
-				texX = 0;
-			if (texY < 0)
-				texY = 0;
-
-			srcRect.x = texX;
-			srcRect.y = texY;
-			srcRect.w = 1;
-			srcRect.h = 1;
-
-			dstRect.x = x;
-			dstRect.y = y;
-			dstRect.w = 1;
-			dstRect.h = 1;
-
-			SDL_RenderCopy(renderer, floorTexture.texture, &srcRect, &dstRect);
-		}
-	}
-}
-
-/**
  * draw_map_tiles - Draws the tiles of the mini-map based on the maze map.
  * @mapStartX: The starting x-coordinate for the map on the screen.
  * @mapStartY: The starting y-coordinate for the map on the screen.
@@ -116,7 +41,7 @@ void draw_map_tiles(int mapStartX, int mapStartY, int tileSize)
 }
 
 /**
- * draw_mini_map - Draws the mini-map, the player and their line of sight.
+ * draw_mini_map - Draws the mini-map, including the player and their line of sight.
  * @player: Pointer to the player's structure containing position and angle.
  * @showMap: Boolean indicating whether to show the map or not.
  *
