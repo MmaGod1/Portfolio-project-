f6f2109c6fb74e357debb9096f6294b2a3513d6b
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 16:12:17 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 #include "raycasting.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     GameStats gameStats;
+
     memset(&gameStats, 0, sizeof(GameStats));
 
     if (argc != 2)
     {
         fprintf(stderr, "Usage: %s <mapfile>\n", argv[0]);
-        return 1;
+        return (1);
     }
 
     /** Initialize SDL */
@@ -16,11 +17,11 @@ int main(int argc, char* argv[])
     {
         fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", 
                 SDL_GetError());
-        return 1;
+        return (1);
     }
 
-    gameStats.window = SDL_CreateWindow("Go-Maze", 
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+    gameStats.window = SDL_CreateWindow("Go-Maze",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
     if (!gameStats.window)
@@ -28,10 +29,10 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Window could not be created! SDL_Error: %s\n", 
                 SDL_GetError());
         SDL_Quit();
-        return 1;
+        return (1);
     }
 
-    gameStats.renderer = SDL_CreateRenderer(gameStats.window, -1, 
+    gameStats.renderer = SDL_CreateRenderer(gameStats.window, -1,
         SDL_RENDERER_ACCELERATED);
     
     if (!gameStats.renderer)
@@ -40,32 +41,37 @@ int main(int argc, char* argv[])
                 SDL_GetError());
         SDL_DestroyWindow(gameStats.window);
         SDL_Quit();
-        return 1;
+        return (1);
     }
 
-    Player player = { .x = 2.0, .y = 2.0, .angle = 0.0, 
+    Player player = { .x = 2.0, .y = 2.0, .angle = 0.0,
                       .moveSpeed = 0.05, .rotSpeed = 0.05 };
 
-    if (load_map(argv[1], gameStats.maze_map) != 0)
+    if (load_map(&gameStats, argv[1]) != 0)
     {
         fprintf(stderr, "Failed to load map\n");
         SDL_DestroyRenderer(gameStats.renderer);
         SDL_DestroyWindow(gameStats.window);
         SDL_Quit();
-        return 1;
+        return (1);
     }
 
-    gameStats.wallTextures[0] = load_texture("./images/wall1.jpg");
-    gameStats.wallTextures[1] = load_texture("./images/wall2.jpg");
-    gameStats.wallTextures[2] = load_texture("./images/wall3.jpg");
-    gameStats.wallTextures[3] = load_texture("./images/wall4.jpg");
-    gameStats.floorTexture = load_texture("./images/floor.jpg");
+    gameStats.wallTextures[0] = load_texture(&gameStats, 
+        "./images/wall1.jpg");
+    gameStats.wallTextures[1] = load_texture(&gameStats, 
+        "./images/wall2.jpg");
+    gameStats.wallTextures[2] = load_texture(&gameStats, 
+        "./images/wall3.jpg");
+    gameStats.wallTextures[3] = load_texture(&gameStats, 
+        "./images/wall4.jpg");
+    gameStats.floorTexture = load_texture(&gameStats, 
+        "./images/floor.jpg");
 
     /** Game loop */
     bool running = true;
     while (running)
     {
-        handle_input(&player, &running, gameStats.maze_map);
+        handle_input(&player, &running, &gameStats);
         render(&player, &gameStats);
         SDL_Delay(16);
     }
