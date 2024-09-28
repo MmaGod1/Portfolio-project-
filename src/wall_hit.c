8031f2b01b0c979c3a937e4b5ed71a6a9a5fdd5c
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 18:11:55 +0100

    Update wall_hit.c

--- src/wall_hit.c
+++ src/wall_hit.c
@@ -1,54 +1,55 @@
 #include "raycasting.h"
 
-float get_wall_hit_coordinates(GameStats *gameStats, float playerX,
-                               float playerY, float rayAngle,
-                               int *mapX, int *mapY)
+static void initialize_step_and_side(float rayDirX, float rayDirY,
+                                      float playerX, float playerY,
+                                      int *mapX, int *mapY,
+                                      int *stepX, int *stepY,
+                                      float *sideDistX, float *sideDistY)
 {
-    float rayDirX = cos(rayAngle);
-    float rayDirY = sin(rayAngle);
-
     *mapX = (int)playerX;
     *mapY = (int)playerY;
 
-    float sideDistX, sideDistY, wallX;
-    float deltaDistX = fabs(1 / rayDirX);
-    float deltaDistY = fabs(1 / rayDirY);
-
-    int stepX, stepY, side, hit = 0;
-
     if (rayDirX < 0)
     {
-        stepX = -1;
-        sideDistX = (playerX - *mapX) * deltaDistX;
+        *stepX = -1;
+        *sideDistX = (playerX - *mapX) * fabs(1 / rayDirX);
     }
     else
     {
-        stepX = 1;
-        sideDistX = (*mapX + 1.0 - playerX) * deltaDistX;
+        *stepX = 1;
+        *sideDistX = (*mapX + 1.0 - playerX) * fabs(1 / rayDirX);
     }
 
     if (rayDirY < 0)
     {
-        stepY = -1;
-        sideDistY = (playerY - *mapY) * deltaDistY;
+        *stepY = -1;
+        *sideDistY = (playerY - *mapY) * fabs(1 / rayDirY);
     }
     else
     {
-        stepY = 1;
-        sideDistY = (*mapY + 1.0 - playerY) * deltaDistY;
+        *stepY = 1;
+        *sideDistY = (*mapY + 1.0 - playerY) * fabs(1 / rayDirY);
     }
+}
+
+static int perform_dda(GameStats *gameStats, int *mapX, int *mapY,
+                        int stepX, int stepY, float sideDistX,
+                        float sideDistY)
+{
+    int hit = 0;
+    int side;
 
     while (hit == 0)
     {
         if (sideDistX < sideDistY)
         {
-            sideDistX += deltaDistX;
+            sideDistX += fabs(1 / rayDirX);
             *mapX += stepX;
             side = 0;
         }
         else
         {
-            sideDistY += deltaDistY;
+            sideDistY += fabs(1 / rayDirY);
             *mapY += stepY;
             side = 1;
         }
@@ -59,17 +60,35 @@ float get_wall_hit_coordinates(GameStats *gameStats, float playerX,
             hit = 1;
         }
     }
+    return side;
+}
 
+float get_wall_hit_coordinates(GameStats *gameStats, float playerX,
+                               float playerY, float rayAngle,
+                               int *mapX, int *mapY)
+{
+    float rayDirX = cos(rayAngle);
+    float rayDirY = sin(rayAngle);
+
+    int stepX, stepY;
+    float sideDistX, sideDistY;
+    
+    initialize_step_and_side(rayDirX, rayDirY, playerX, playerY, mapX, mapY,
+                              &stepX, &stepY, &sideDistX, &sideDistY);
+
+    int side = perform_dda(gameStats, mapX, mapY, stepX, stepY,
+                            sideDistX, sideDistY);
+
+    float wallX;
     if (side == 0)
     {
-        wallX = playerY + ((sideDistX - deltaDistX) * rayDirY);
+        wallX = playerY + ((sideDistX - fabs(1 / rayDirX)) * rayDirY);
     }
     else
     {
-        wallX = playerX + ((sideDistY - deltaDistY) * rayDirX);
+        wallX = playerX + ((sideDistY - fabs(1 / rayDirY)) * rayDirX);
     }
 
     wallX -= floor(wallX);
-
     return (wallX);
 }
