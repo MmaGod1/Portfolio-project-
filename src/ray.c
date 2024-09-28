6514244ed3bccb557eccfd5e3b1916de55397cac
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 14:58:48 +0100

    Update ray.c

--- src/ray.c
+++ src/ray.c
@@ -1,46 +1,42 @@
-#include "raycasting.h"
-
-float cast_ray(float playerX, float playerY, float rayAngle)
+float cast_ray(GameStats *gameStats, float playerX, float playerY, 
+                float rayAngle)
 {
-int stepX, stepY, side;
-float sideDistX, sideDistY;
-/* Ray direction */
-float rayDirX = cos(rayAngle);
-float rayDirY = sin(rayAngle);
+    int stepX, stepY, side;
+    float sideDistX, sideDistY;
+    float rayDirX = cos(rayAngle);
+    float rayDirY = sin(rayAngle);
 
-int mapX = (int)playerX;
-int mapY = (int)playerY;
+    int mapX = (int)playerX;
+    int mapY = (int)playerY;
 
-float deltaDistX = fabs(1 / rayDirX);
-float deltaDistY = fabs(1 / rayDirY);
-bool hit = false;
+    float deltaDistX = fabs(1 / rayDirX);
+    float deltaDistY = fabs(1 / rayDirY);
+    bool hit = false;
 
-/* Calculate step and initial sideDist */
-if (rayDirX < 0)
-{
-stepX = -1;
+    if (rayDirX < 0)
+    {
+        stepX = -1;
         sideDistX = (playerX - mapX) * deltaDistX;
-    } 
+    }
     else
-{
+    {
         stepX = 1;
         sideDistX = (mapX + 1.0 - playerX) * deltaDistX;
     }
+    
     if (rayDirY < 0)
-{
+    {
         stepY = -1;
         sideDistY = (playerY - mapY) * deltaDistY;
-}
-else
-{
+    }
+    else
+    {
         stepY = 1;
         sideDistY = (mapY + 1.0 - playerY) * deltaDistY;
     }
 
-    /* Perform DDA */
     while (!hit)
-{
-        /* Jump to next map square, either in x-direction or y-direction */
+    {
         if (sideDistX < sideDistY)
         {
             sideDistX += deltaDistX;
@@ -54,14 +50,13 @@ else
             side = 1;
         }
 
-        /* Check if ray has hit a wall */
-        if (mapX >= 0 && mapX < MAP_WIDTH && mapY >= 0 && mapY < MAP_HEIGHT && maze_map[mapX][mapY] == 1)
+        if (mapX >= 0 && mapX < MAP_WIDTH && mapY >= 0 && mapY < MAP_HEIGHT && 
+            gameStats->maze_map[mapX][mapY] == 1)
         {
             hit = true;
         }
     }
 
-    /*Calculate the perpendicular wall distance for correction */
     float perpWallDist;
     if (side == 0)
     {
