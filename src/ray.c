64678cf5310e1fc90d799f67a86d9da842595fec
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 17:40:31 +0100

    Update ray.c

--- src/ray.c
+++ src/ray.c
@@ -1,21 +1,20 @@
 #include "raycasting.h"
 
-void initialize_ray(float rayAngle, float *rayDirX, float *rayDirY, 
-                    int *stepX, int *stepY, float playerX, float playerY,
-                    int *mapX, int *mapY, float *sideDistX, 
-                    float *sideDistY, float *deltaDistX, 
-                    float *deltaDistY)
+void initialize_ray(float playerX, float playerY, float rayAngle,
+                    int *mapX, int *mapY, int *stepX, int *stepY,
+                    float *sideDistX, float *sideDistY,
+                    float *deltaDistX, float *deltaDistY)
 {
-    *rayDirX = cos(rayAngle);
-    *rayDirY = sin(rayAngle);
+    float rayDirX = cos(rayAngle);
+    float rayDirY = sin(rayAngle);
 
     *mapX = (int)playerX;
     *mapY = (int)playerY;
 
-    *deltaDistX = fabs(1 / (*rayDirX));
-    *deltaDistY = fabs(1 / (*rayDirY));
+    *deltaDistX = fabs(1 / rayDirX);
+    *deltaDistY = fabs(1 / rayDirY);
 
-    if (*rayDirX < 0)
+    if (rayDirX < 0)
     {
         *stepX = -1;
         *sideDistX = (playerX - *mapX) * (*deltaDistX);
@@ -26,7 +25,7 @@ void initialize_ray(float rayAngle, float *rayDirX, float *rayDirY,
         *sideDistX = (*mapX + 1.0 - playerX) * (*deltaDistX);
     }
 
-    if (*rayDirY < 0)
+    if (rayDirY < 0)
     {
         *stepY = -1;
         *sideDistY = (playerY - *mapY) * (*deltaDistY);
@@ -38,64 +37,60 @@ void initialize_ray(float rayAngle, float *rayDirX, float *rayDirY,
     }
 }
 
-float calculate_distance(int stepX, int stepY, int *mapX, int *mapY, 
-                         float playerX, float playerY, 
-                         float rayDirX, float rayDirY, 
-                         GameStats *gameStats)
+float perform_ray_cast(GameStats *gameStats, int *mapX, int *mapY,
+                       int stepX, int stepY, float sideDistX,
+                       float sideDistY, float deltaDistX, float deltaDistY,
+                       float playerX, float playerY)
 {
-    float sideDistX = 0;
-    float sideDistY = 0;
-    int side = 0;
+    int side;
     bool hit = false;
 
     while (!hit)
     {
         if (sideDistX < sideDistY)
         {
-            sideDistX += fabs(1 / rayDirX);
+            sideDistX += deltaDistX;
             *mapX += stepX;
             side = 0;
         }
         else
         {
-            sideDistY += fabs(1 / rayDirY);
+            sideDistY += deltaDistY;
             *mapY += stepY;
             side = 1;
         }
 
-        if (*mapX >= 0 && *mapX < MAP_WIDTH && *mapY >= 0 && *mapY < MAP_HEIGHT && 
-            gameStats->maze_map[*mapX][*mapY] == 1)
+        if (*mapX >= 0 && *mapX < MAP_WIDTH && *mapY >= 0 &&
+            *mapY < MAP_HEIGHT && gameStats->maze_map[*mapX][*mapY] == 1)
+        {
             hit = true;
+        }
     }
 
     float perpWallDist;
     if (side == 0)
     {
-        perpWallDist = (*mapX - playerX + (1 - stepX) / 2) / rayDirX;
+        perpWallDist = (*mapX - playerX + (1 - stepX) / 2) / cos(rayAngle);
     }
     else
     {
-        perpWallDist = (*mapY - playerY + (1 - stepY) / 2) / rayDirY;
+        perpWallDist = (*mapY - playerY + (1 - stepY) / 2) / sin(rayAngle);
     }
 
     return (perpWallDist);
 }
 
-float cast_ray(GameStats *gameStats, float playerX, float playerY, 
+float cast_ray(GameStats *gameStats, float playerX, float playerY,
                 float rayAngle)
 {
-    float rayDirX, rayDirY;
-    int stepX, stepY;
-    int mapX, mapY;
-    float sideDistX, sideDistY;
-    float deltaDistX, deltaDistY;
+    int stepX, stepY, mapX, mapY;
+    float sideDistX, sideDistY, deltaDistX, deltaDistY;
 
-    initialize_ray(rayAngle, &rayDirX, &rayDirY, &stepX, &stepY, 
-                   playerX, playerY, &mapX, &mapY, 
-                   &sideDistX, &sideDistY, &deltaDistX, &deltaDistY);
-    
-    return (calculate_distance(stepX, stepY, &mapX, &mapY, 
-                               playerX, playerY, 
-                               rayDirX, rayDirY, 
-                               gameStats));
+    initialize_ray(playerX, playerY, rayAngle, &mapX, &mapY,
+                   &stepX, &stepY, &sideDistX, &sideDistY,
+                   &deltaDistX, &deltaDistY);
+
+    return (perform_ray_cast(gameStats, &mapX, &mapY, stepX, stepY,
+                             sideDistX, sideDistY, deltaDistX,
+                             deltaDistY, playerX, playerY));
 }
