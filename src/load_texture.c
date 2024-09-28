0a1151e72b508b392fc1e333547b39352062ec90
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 14:47:39 +0100

    Update load_texture.c

--- src/load_texture.c
+++ src/load_texture.c
@@ -1,17 +1,20 @@
 #include "raycasting.h"
 
-Texture load_texture(const char *filename)
+Texture load_texture(GameStats *gameStats, const char *filename)
 {
     SDL_Surface *surface = IMG_Load(filename);
+    Texture texture;
+
     if (!surface)
     {
         fprintf(stderr, "Error loading texture: %s\n", IMG_GetError());
         return (Texture){NULL, 0, 0};
     }
-    Texture texture;
-    texture.texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    texture.texture = SDL_CreateTextureFromSurface(gameStats->renderer, surface);
     SDL_FreeSurface(surface);
     texture.width = surface->w;
     texture.height = surface->h;
+
     return (texture);
 }
