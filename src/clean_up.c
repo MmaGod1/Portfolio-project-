c38bb61ce04e8f551f299ce40e99f785acdebd1d
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 22:22:10 +0100

    Update clean_up.c

--- src/clean_up.c
+++ src/clean_up.c
@@ -1,5 +1,14 @@
 #include "raycasting.h"
 
+
+/**
+ * clean_up - Frees textures, renderer, window, and quits SDL.
+ *
+ * @gameStats: Pointer to the GameStats struct containing the SDL resources.
+ * 
+ * Frees any dynamically allocated textures in the game, destroys the
+ * renderer and window, and shuts down SDL to release all resources.
+ */
 void clean_up(GameStats *gameStats)
 {
     for (int i = 0; i < 4; i++)
