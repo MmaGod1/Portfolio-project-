a13a2aa380139acb710d62054dc41617d9d478b5
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 18:33:31 +0100

    Update raycasting.h

--- inc/raycasting.h
+++ inc/raycasting.h
@@ -100,15 +100,20 @@ float cast_ray(GameStats *gameStats, float playerX, float playerY,
 
 
 
-static void initialize_side_distances(float playerX, float playerY,
-                                      float rayDirX, float rayDirY,
-                                      int *mapX, int *mapY,
-                                      int *stepX, int *stepY,
-                                      float *sideDistX, float *sideDistY);
-
-static int perform_dda(GameStats *gameStats, int *mapX, int *mapY,
-                       float *sideDistX, float *sideDistY,
-                       int stepX, int stepY, float deltaDistX, float deltaDistY);
+void init_ray_direction(float *rayDirX, float *rayDirY, float rayAngle);
+void init_map_coordinates(float playerX, float playerY, int *mapX, int *mapY);
+void calculate_step_and_initial_side_dist(float playerX, float playerY,
+                                          int *mapX, int *mapY,
+                                          float rayDirX, float rayDirY,
+                                          int *stepX, int *stepY,
+                                          float *sideDistX, float *sideDistY,
+                                          float deltaDistX, float deltaDistY);
+int perform_dda(int *mapX, int *mapY, float *sideDistX, float *sideDistY,
+                float deltaDistX, float deltaDistY, int stepX, int stepY,
+                int *side, GameStats *gameStats);
+float get_wall_hit_coordinates(GameStats *gameStats, float playerX,
+                               float playerY, float rayAngle,
+                               int *mapX, int *mapY);
 
 float get_wall_hit_coordinates(GameStats *gameStats, float playerX, 
                                float playerY, float rayAngle, 
