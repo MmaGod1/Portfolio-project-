bf9f46584a556b3b39673920b620de023558f048
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 12:02:02 +0100

    Update draw.c

--- src/draw.c
+++ src/draw.c
@@ -8,6 +8,32 @@ void draw_sky()
 }
 
 
+void draw_floor(Player *player)
+{
+    for (int y = SCREEN_HEIGHT / 2; y < SCREEN_HEIGHT; y++)
+    {
+        float floorDist = SCREEN_HEIGHT / (2.0f * y - SCREEN_HEIGHT); // Perspective correct distance
+
+        for (int x = 0; x < SCREEN_WIDTH; x++)
+        {
+            float rayAngle = player->angle - (FOV / 2) + (FOV * x / SCREEN_WIDTH);
+            float floorX = player->x + cos(rayAngle) * floorDist;
+            float floorY = player->y + sin(rayAngle) * floorDist;
+
+            int texX = (int)(floorX * floorTexture.width) % floorTexture.width;
+            int texY = (int)(floorY * floorTexture.height) % floorTexture.height;
+            if (texX < 0)
+                texX = 0;
+            if (texY < 0)
+                texY = 0;
+
+            /* Render the floor pixel */
+            SDL_Rect srcRect = { texX, texY, 1, 1 };
+            SDL_Rect dstRect = { x, y, 1, 1 };
+            SDL_RenderCopy(renderer, floorTexture.texture, &srcRect, &dstRect);
+        }
+    }
+}
 
 void draw_mini_map(Player *player, bool showMap)
 {
