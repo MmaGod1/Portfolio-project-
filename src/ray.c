4c3d73c25977b31cc2c541a29101097f95471c63
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 11:35:14 +0100

    Update ray.c

--- src/ray.c
+++ src/ray.c
@@ -1,56 +1,76 @@
 #include "raycasting.h"
 
-float cast_ray(float playerX, float playerY, float rayAngle)
+float castRay(float playerX, float playerY, float rayAngle)
 {
+int stepX, stepY, side;
+float sideDistX, sideDistY;
 /* Ray direction */
 float rayDirX = cos(rayAngle);
-    float rayDirY = sin(rayAngle);
+float rayDirY = sin(rayAngle);
 
-    // Which box of the map we're in
-    int mapX = (int)playerX;
-    int mapY = (int)playerY;
+int mapX = (int)playerX;
+int mapY = (int)playerY;
 
-    // Length of ray from one x or y side to next x or y side
-    float deltaDistX = fabs(1 / rayDirX);
-    float deltaDistY = fabs(1 / rayDirY);
+float deltaDistX = fabs(1 / rayDirX);
+float deltaDistY = fabs(1 / rayDirY);
+bool hit = false;
 
-    // What direction to step in (+1 or -1)
-    int stepX, stepY;
-    float sideDistX, sideDistY;
-
-    // Calculate step and initial sideDist
-    if (rayDirX < 0) {
-        stepX = -1;
+/* Calculate step and initial sideDist */
+if (rayDirX < 0)
+{
+stepX = -1;
         sideDistX = (playerX - mapX) * deltaDistX;
-    } else {
+    } 
+    else
+{
         stepX = 1;
         sideDistX = (mapX + 1.0 - playerX) * deltaDistX;
     }
-    if (rayDirY < 0) {
+    if (rayDirY < 0)
+{
         stepY = -1;
         sideDistY = (playerY - mapY) * deltaDistY;
-    } else {
+}
+else
+p{
         stepY = 1;
         sideDistY = (mapY + 1.0 - playerY) * deltaDistY;
     }
 
-    // Perform DDA
-    bool hit = false;
-    int side; // was the wall hit on the X or Y side?
-    while (!hit) {
-        // Jump to next map square, either in x-direction or y-direction
-        if (sideDistX < sideDistY) {
+    /* Perform DDA */
+    while (!hit)
+{
+        /* Jump to next map square, either in x-direction or y-direction */
+        if (sideDistX < sideDistY)
+        {
             sideDistX += deltaDistX;
             mapX += stepX;
-            side = 0; // X-side hit
-        } else {
+            side = 0;
+        }
+        else
+        {
             sideDistY += deltaDistY;
             mapY += stepY;
-            side = 1; // Y-side hit
+            side = 1;
         }
 
-        // Check if ray has hit a wall
-        if (mapX >= 0 && mapX < MAP_WIDTH && mapY >= 0 && mapY < MAP_HEIGHT && maze_map[mapX][mapY] == 1) {
+        /* Check if ray has hit a wall */
+        if (mapX >= 0 && mapX < MAP_WIDTH && mapY >= 0 && mapY < MAP_HEIGHT && maze_map[mapX][mapY] == 1)
+        {
             hit = true;
         }
+    }
+
+    / *Calculate the perpendicular wall distance for correction */
+    float perpWallDist;
+    if (side == 0)
+    {
+        perpWallDist = (mapX - playerX + (1 - stepX) / 2) / rayDirX;
+    }
+    else
+    {
+        perpWallDist = (mapY - playerY + (1 - stepY) / 2) / rayDirY;
+    }
+
+    return (perpWallDist);
 }
