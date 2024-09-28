19487b724caeae0809d28c0047d7defd15572d4c
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-28 14:46:11 +0100

    Update input_events.c

--- src/input_events.c
+++ src/input_events.c
@@ -1,17 +1,14 @@
 #include "raycasting.h"
 
-void handle_input(Player *player, bool *running, int maze_map[MAP_WIDTH][MAP_HEIGHT])
+void handle_input(Player *player, bool *running, GameStats *gameStats)
 {
     SDL_Event event;
-    float newX;
-    float newY;
+    float newX, newY;
 
     while (SDL_PollEvent(&event))
     {
         if (event.type == SDL_QUIT)
-        {
             *running = false;
-        }
 
         if (event.type == SDL_KEYDOWN)
         {
@@ -23,47 +20,35 @@ void handle_input(Player *player, bool *running, int maze_map[MAP_WIDTH][MAP_HEI
                 case SDLK_l:
                     *running = false;
                     break;
-
                 case SDLK_m:
-                    showMap = !showMap;
+                    gameStats->showMap = !gameStats->showMap;
                     break;
-
                 case SDLK_LEFT:
                     player->angle -= player->rotSpeed;
                     if (player->angle < 0)
-                    {
                         player->angle += 2 * M_PI;
-                    }
                     break;
-
                 case SDLK_RIGHT:
                     player->angle += player->rotSpeed;
                     if (player->angle > 2 * M_PI)
-                    {
                         player->angle -= 2 * M_PI;
-                    }
                     break;
-
                 case SDLK_w:
                     newX = player->x + cos(moveAngle) * moveStep;
                     newY = player->y + sin(moveAngle) * moveStep;
                     break;
-
                 case SDLK_s:
                     newX = player->x - cos(moveAngle) * moveStep;
                     newY = player->y - sin(moveAngle) * moveStep;
                     break;
-
                 case SDLK_a:
                     newX = player->x + cos(moveAngle - M_PI / 2) * moveStep;
                     newY = player->y + sin(moveAngle - M_PI / 2) * moveStep;
                     break;
-
                 case SDLK_d:
                     newX = player->x + cos(moveAngle + M_PI / 2) * moveStep;
                     newY = player->y + sin(moveAngle + M_PI / 2) * moveStep;
                     break;
-
                 default:
                     continue;
             }
@@ -71,7 +56,7 @@ void handle_input(Player *player, bool *running, int maze_map[MAP_WIDTH][MAP_HEI
             /* Perform collision detection once, checking both newX and newY */
             if (newX >= 0 && newX < MAP_WIDTH && newY >= 0 && newY < MAP_HEIGHT)
             {
-                if (maze_map[(int)newX][(int)newY] == 0)
+                if (gameStats->maze_map[(int)newX][(int)newY] == 0)
                 {
                     player->x = newX;
                     player->y = newY;
