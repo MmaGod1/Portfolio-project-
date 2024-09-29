9ef64f37cf6c13128e8877b2ab0374f9c21d70b2
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-29 13:51:30 +0100

    Update ray.c

--- src/ray.c
+++ src/ray.c
@@ -121,7 +121,6 @@ void initialize_ray(float playerX, float playerY, float rayAngle,
 */
 
 float perform_ray_cast(GameStats *gameStats, int *mapX, int *mapY,
-float perform_ray_cast(GameStats *gameStats, int *mapX, int *mapY,
                        int stepX, int stepY, float sideDistX,
                        float sideDistY, float deltaDistX, float deltaDistY,
                        float playerX, float playerY, float rayAngle)
