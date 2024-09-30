be78528712bdeb924c17e23e3bfc90aefb0ec94c
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-30 11:41:04 +0100

    Update raycasting.h

--- inc/raycasting.h
+++ inc/raycasting.h
@@ -112,14 +112,13 @@ void calculate_step_and_initial_side_dist(float playerX, float playerY,
 int dda_raycast_step(int *mapX, int *mapY, float *sideDistX, float *sideDistY,
                 float deltaDistX, float deltaDistY, int stepX, int stepY,
                 int *side, GameStats *gameStats);
+
 float calculate_wall_x(float playerX, float playerY, int side,
-                       float sideDistX, float sideDistY,
-                       float deltaDistX, float deltaDistY,
                        float rayDirX, float rayDirY,
                        int mapX, int mapY, int stepX, int stepY);
 
-float get_wall_hit_coordinates(GameStats *gameStats, float playerX, 
-                               float playerY, float rayAngle, 
+float get_wall_hit_coordinates(GameStats *gameStats, float playerX,
+                               float playerY, float rayAngle,
                                int *mapX, int *mapY);
 
 
